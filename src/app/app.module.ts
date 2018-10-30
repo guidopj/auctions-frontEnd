3db8b641f8efc109c1d1f348d13da69ba45b4b13
guido <guidopujadas@gmail.com>
2018-10-30 17:01:10 -0300

    Menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
-import { MenuComponent } from './menu/menu.component';
+import { AuthMenuComponent } from './auth-menu/auth-menu.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AuctionCreationComponent } from './auction-creation/auction-creation.component';
+import { GuestMenuComponent } from './guest-menu/guest-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterUserComponent,
-    MenuComponent,
+    AuthMenuComponent,
     LoginComponent,
-    AuctionCreationComponent
+    AuctionCreationComponent,
+    GuestMenuComponent
   ],
   imports: [
     BrowserModule,
